ce2ae48dcf7608de459f3ca22a54519962285955
Jakub Mestek <jakub.mestek@outlook.cz>
2021-05-23 22:42:27 +0200

    Add project files.

---
+++ FormulaEncoderDecoder/FormulaImplementation.cs
@@ -0,0 +1,67 @@
+﻿using SatDotNet.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace SatDotNet.FormulaEncoderDecoder
+{
+    class Variable : IVariable
+    {
+        public Variable()
+        {
+            NegativeLiteral = new Literal(this, false);
+            PositiveLiteral = new Literal(this, true);
+        }
+
+        public bool Equals(IVariable other)
+        {
+            return object.ReferenceEquals(this, other);
+        }
+
+        public ILiteral NegativeLiteral { get; }
+
+        public ILiteral PositiveLiteral { get; }
+    }
+
+    class Literal : ILiteral
+    {
+        public Literal(IVariable variable, bool isPositive)
+        {
+            Variable = variable ?? throw new ArgumentNullException(nameof(variable));
+            IsPositive = isPositive;
+        }
+
+        public IVariable Variable { get; private set; }
+
+        public bool IsPositive { get; private set; }
+
+        public bool Equals(ILiteral other)
+        {
+            return this.Variable.Equals(other.Variable) && this.IsPositive == other.IsPositive;
+        }
+
+        public ILiteral Negate()
+        {
+            return IsPositive ? Variable.NegativeLiteral : Variable.PositiveLiteral;
+        }
+    }
+
+    class Clause : IClause
+    {
+        public Clause(IEnumerable<ILiteral> literals)
+        {
+            Literals = literals ?? throw new ArgumentNullException(nameof(literals));
+        }
+
+        public IEnumerable<ILiteral> Literals { get; }
+    }
+
+    class CnfFormula : ICnfFormula
+    {
+        public CnfFormula(IEnumerable<IClause> clauses)
+        {
+            Clauses = clauses ?? throw new ArgumentNullException(nameof(clauses));
+        }
+
+        public IEnumerable<IClause> Clauses { get; }
+    }
+}
---
+++ Interfaces/FormulaInterfaces.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SatDotNet.Interfaces
+{
+    public interface IVariable : IEquatable<IVariable>
+    {
+        ILiteral PositiveLiteral { get; }
+        ILiteral NegativeLiteral { get; }
+    }
+
+    public interface ILiteral : IEquatable<ILiteral>
+    {
+        IVariable Variable { get; }
+        bool IsPositive { get; }
+        ILiteral Negate();
+    }
+
+    public interface IClause
+    {
+        IEnumerable<ILiteral> Literals { get; }
+    }
+
+    public interface ICnfFormula
+    {
+        IEnumerable<IClause> Clauses { get; }
+    }
+
+}
